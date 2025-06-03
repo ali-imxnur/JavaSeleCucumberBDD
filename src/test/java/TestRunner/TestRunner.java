91e35298b87dc9dfedee2c2010daff19f636c1dc
Imanur Ali <imanur.8670@gmail.com>
2025-06-03 12:01:20 +0530

    Second commit

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,11 +1,9 @@
 package TestRunner;
 
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
 		  features = {
 		    "src/test/resources/Features/demoblazeEcommerce.feature",
@@ -20,7 +18,7 @@ import io.cucumber.junit.CucumberOptions;
 		  monochrome = true
 		)
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 	
 	/*this class will be empty */
 }
